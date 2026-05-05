a0f5b030da9ecc3abd936088c373f7b74f573056
realTaharial <t.eneserden2@gmail.com>
2026-05-06 02:05:30 +0300

    a

--- assignment/assignment.ino
+++ assignment/assignment.ino
@@ -9,7 +9,7 @@
 // ================= ADAFRUIT IO AYARLARI =================
 
 #define IO_USERNAME  "taharial"
-#define IO_KEY       "aio_Ozph62iFRxaSjlCwzUVrd5DgG3OL"
+#define IO_KEY       "a"
 
 #define WIFI_SSID    "taha enes"
 #define WIFI_PASS    "taha1234"
